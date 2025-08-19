7bd4cb37e4213e5e017abce0bbb3c0611661e50c
MarksWorkspace <markbrooks1207@gmail.com>
2025-08-19 14:52:46 -0500

    added route to new neighborhood button

--- my-react-app/src/components/NewNeighborhoodButton/NewNeighborhoodButton.js
+++ my-react-app/src/components/NewNeighborhoodButton/NewNeighborhoodButton.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 import './NewNeighborhoodButton.css';
 
 const NewNeighborhoodButton = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/create-campaign');
+  };
+
   return (
-    <button className="new-neighborhood-btn">
+    <button className="new-neighborhood-btn" onClick={handleClick}>
       <FiPlus size={12} />
       New Neighborhood
     </button>
